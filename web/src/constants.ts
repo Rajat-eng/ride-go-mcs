ec26921d91cc401fff91dbcbd8cca5508dcb2053
Rajat-Eng <vrajat269@gmail.com>
2026-06-19 15:32:07 +0530

    change prod urls's

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,2 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8081';
-export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL ?? 'ws://localhost:8082/ws';
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? (isProduction ? 'https://api.rexy.co.in' : 'http://localhost:8081');
+export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL ?? (isProduction ? 'wss://ws.rexy.co.in/ws' : 'ws://localhost:8082/ws');
